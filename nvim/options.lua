0e62a1bfff5a935e6ade6108dd303b8e085919bc
Glen Ernstrom <otterbrain@protonmail.com>
2025-06-18 09:43:36 -0400

    lua configs

---
+++ nvim/options.lua
@@ -0,0 +1,17 @@
+vim.o.number = true
+vim.o.relativenumber = true
+
+vim.o.clipboard = 'unnamedplus'
+
+vim.o.signcolumn = 'yes'
+
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+
+vim.o.termguicolors = true
+
+vim.o.updatetime = 300
+
+vim.o.mouse = 'a'
+
+
