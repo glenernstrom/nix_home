eafc3a7d543bcb291b9f3184fbb8de03bc5eaf27
Glen Ernstrom <otterbrain@protonmail.com>
2025-06-18 09:43:20 -0400

    added inputs

--- home.nix
+++ home.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, pkgs, inputs, ... }:
 
 {
   # Home Manager needs a bit of information about you and the paths it should
@@ -71,6 +71,91 @@
     # EDITOR = "emacs";
   };
 
+  # Let's configure neovim!
+
+  programs.neovim =
+  let
+    toLua = str: "lua << EOF\n${str}\nEOF\n";
+    toLuaFile = file: "lua <<EOF\n${builtins.readFile file}\nEOF\n";
+  in
+  {
+    enable = true;
+
+    viAlias = true;
+    vimAlias = true;
+    vimdiffAlias = true;
+
+     extraPackages = with pkgs; [
+      lua-language-server
+      rnix-lsp
+
+      wl-clipboard
+    ];
+    
+    extraLuaConfig = ''
+    
+    ${builtins.readFile ./nvim/options.lua}
+
+    '';
+
+    plugins = with pkgs.vimPlugins; [
+      {
+        plugin = comment-nvim;
+        config = toLua "require(\"Comment\").setup()";
+      }
+
+      {
+        plugin = nvim-lspconfig;
+        config = toLuaFile ./nvim/plugin/lsp.lua;
+      }
+
+      {
+        plugin = gruvbox-nvim;
+        config = "colorscheme gruvbox";
+      }
+
+      neodev-nvim
+      
+      nvim-cmp
+      {
+        plugin = nvim-cmp;
+        config = toLuaFile ./nvim/plugin/cmp.lua;
+      }
+
+      {
+        plugin = telescope-nvim;
+        config = toLuaFile ./nvim/plugin/telescope.lua;
+      }
+
+      telescope-fzf-native-nvim
+
+      cmp_luasnip
+      cmp-nvim-lsp
+
+      luasnip
+      friendly-snippets
+
+      lualine-nvim
+      nvim-web-devicons
+
+      {
+        plugin = (nvim-treesitter.withPlugins (p: [
+          p.tree-sitter-nix
+          p.tree-sitter-vim
+          p.tree-sitter-bash
+          p.tree-sitter-lua
+          p.tree-sitter-lua
+          p.tree-sitter-python
+          p.tree-sitter-jason
+        ]));
+      }
+
+      vim-nix
+      
+    ];
+    
+  };
+          
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
