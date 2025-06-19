eb3092962f8fd3344b12d076bb283e91d9ca22eb
Glen Ernstrom <otterbrain@protonmail.com>
2025-06-19 10:00:51 -0400

    cleaned up

--- home.nix
+++ home.nix
@@ -74,10 +74,10 @@
   # Let's configure neovim!
 
   programs.neovim =
-  let
-    toLua = str: "lua << EOF\n${str}\nEOF\n";
-    toLuaFile = file: "lua <<EOF\n${builtins.readFile file}\nEOF\n";
-  in
+  # let
+  #  toLua = str: "lua << EOF\n${str}\nEOF\n";
+  #  toLuaFile = file: "lua <<EOF\n${builtins.readFile file}\nEOF\n";
+  # in
   {
     enable = true;
 
@@ -86,72 +86,17 @@
     vimdiffAlias = true;
 
      extraPackages = with pkgs; [
-      lua-language-server
-      rnix-lsp
-
       wl-clipboard
     ];
     
-    extraLuaConfig = ''
-    
-    ${builtins.readFile ./nvim/options.lua}
-
-    '';
 
     plugins = with pkgs.vimPlugins; [
-      {
-        plugin = comment-nvim;
-        config = toLua "require(\"Comment\").setup()";
-      }
-
-      {
-        plugin = nvim-lspconfig;
-        config = toLuaFile ./nvim/plugin/lsp.lua;
-      }
 
       {
         plugin = gruvbox-nvim;
         config = "colorscheme gruvbox";
       }
 
-      neodev-nvim
-      
-      nvim-cmp
-      {
-        plugin = nvim-cmp;
-        config = toLuaFile ./nvim/plugin/cmp.lua;
-      }
-
-      {
-        plugin = telescope-nvim;
-        config = toLuaFile ./nvim/plugin/telescope.lua;
-      }
-
-      telescope-fzf-native-nvim
-
-      cmp_luasnip
-      cmp-nvim-lsp
-
-      luasnip
-      friendly-snippets
-
-      lualine-nvim
-      nvim-web-devicons
-
-      {
-        plugin = (nvim-treesitter.withPlugins (p: [
-          p.tree-sitter-nix
-          p.tree-sitter-vim
-          p.tree-sitter-bash
-          p.tree-sitter-lua
-          p.tree-sitter-lua
-          p.tree-sitter-python
-          p.tree-sitter-jason
-        ]));
-      }
-
-      vim-nix
-      
     ];
     
   };
