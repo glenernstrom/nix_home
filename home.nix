35565b74cd8c9e5d5ef4cce030f940d93f3de547
Glen Ernstrom <otterbrain@protonmail.com>
2025-06-19 10:08:14 -0400

    added nvim-R edits

--- home.nix
+++ home.nix
@@ -19,7 +19,7 @@
   # environment.
   home = {
     packages = with pkgs; [
-      (import ./nvim-r.nix)
+      (import ./vim.nix)
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
     # pkgs.hello
